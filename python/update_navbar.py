c30509403b2fe4848518d3440c423beed7b450ef
Joshua Smith <32210099+joshuaSmith2021@users.noreply.github.com>
2021-11-16 14:24:47 -0800

    Update navbar, change title to software engineer

--- python/update_navbar.py
+++ python/update_navbar.py
@@ -14,7 +14,11 @@ if check(new_navbar):
     for f in html_files:
         current = open(f'..\{f}').read()
         match = re.search(r'<!-- NAVBAR -->.*<!-- END NAVBAR -->', current, re.DOTALL)
-        new = re.sub(match.group(), f'<!-- NAVBAR -->\n  {new_navbar}\n  <!-- END NAVBAR -->', current)
+        new = None
+        try:
+            new = re.sub(match.group(), f'<!-- NAVBAR -->\n    {new_navbar}\n    <!-- END NAVBAR -->', current)
+        except AttributeError:
+            continue
 
         with open(f'..\{f}', 'w') as file_:
             file_.write(new)
